e560c4e2b34f3eef571f36ddb09c0c7724d9dc98
Ernie Dipko <ernie.dipko@gmail.com>
2019-09-15 08:44:32 -0400

    Initial Commit

---
+++ InventoryScanner/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  InventoryScanner
+//
+//  Created by Ernie Dipko on 9/15/19.
+//  Copyright © 2019 EMDipko, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
